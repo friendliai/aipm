664c48784654cf71cfb46175e8e6d9056d2e6c59
kooyunmo <yunmorning@friendli.ai>
2024-06-05 11:36:09 -0700

    Update system prompt

--- aipm/cli/main.py
+++ aipm/cli/main.py
@@ -33,6 +33,6 @@ def planning(verbose: bool = typer.Option(False)):
 
 
 @app.command()
-def dailysprint(verbose: bool = typer.Option(True)):
+def dailysprint(verbose: bool = typer.Option(False)):
     agent_executor = get_agent(AgentMode.DAILY_SPRINT, verbose=verbose)
     _run_qa_pipeline(agent_executor)
